1d714b50198450414f6f84e9292490e59d563619
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2024-04-03 22:26:14 +0000

    🐛 fix(server): Add CAP_SYS* capabilities to the gpu-screen-recorder (#13)
    ## Description
    
    **What(what issue does this code solve/what feature does it add):**
    
    **How(how does it solve it):**
    
    ## Required Checklist:
    
    - [ ] I have added any necessary documentation and comments in my code
    (where appropriate)
    - [ ] I have added tests to make sure my code runs in all contexts
    
    ## Further comments

--- .scripts/entrypoint.sh
+++ .scripts/entrypoint.sh
@@ -2,3 +2,8 @@
 
 #Start pulseaudio
 /usr/bin/pulseaudio --system --verbose --log-target=stderr --realtime=true --disallow-exit -F /etc/pulse/default.pa
+
+# Attempt to set capabilities
+# Gotten from https://git.dec05eba.com/gpu-screen-recorder/tree/install.sh
+setcap 'cap_sys_admin+ep' /usr/bin/gsr-kms-server
+setcap 'cap_sys_nice+ep' /usr/bin/gpu-screen-recorder
