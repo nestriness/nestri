b636d0da4304470d981395823e50b5d19cdf3579
pure <64907372+pure5665@users.noreply.github.com>
2024-06-01 00:00:45 +0200

    fix startup.sh

--- .scripts/startup.sh
+++ .scripts/startup.sh
@@ -4,20 +4,9 @@ if [ -z "$SESSION_ID" ]; then
     echo "Error: SESSION_ID environment variable is not set."
     exit 1
 fi
-
-xarg="$*"
-
-if [ -z "$xarg" ]; then
-    echo "Error: No command specified to run the game. Exiting."
-    exit 1
-fi
- 
+  
 #Open udp port to listed for QUIC events on 
 export PORT=${PORT:-"8080"}
-
-escaped_xarg=$(printf '%s\n' "$xarg" | sed -e 's/[\/&]/\\&/g')
-
-sudo sed -i "s|^command.*=.*$|command=bash -c \"$escaped_xarg\"|" /etc/supervisord.d/game.ini
 
 sudo sed -i 's|^autostart.*=.*$|autostart=true|' /etc/supervisord.d/game.ini
 
