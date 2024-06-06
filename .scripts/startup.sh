25c29f007e3ace474fb8046121041208647ec75c
pure <64907372+pure5665@users.noreply.github.com>
2024-06-06 23:01:06 +0200

    🐛 fix(server): Fix startup aswell change supervisord's username to use nestri (#82)

--- .scripts/startup.sh
+++ .scripts/startup.sh
@@ -4,10 +4,8 @@ if [ -z "$SESSION_ID" ]; then
     echo "Error: SESSION_ID environment variable is not set."
     exit 1
 fi
-  
+
 #Open udp port to listed for QUIC events on 
 export PORT=${PORT:-"8080"}
-
-sudo sed -i 's|^autostart.*=.*$|autostart=true|' /etc/supervisord.d/game.ini
 
 sudo -E /usr/bin/supervisord -c /etc/supervisord.conf
