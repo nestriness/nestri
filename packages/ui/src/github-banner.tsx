b31c5ad3c8d70d9a6cd528fd9536c2a6f6617014
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2024-08-31 19:56:41 +0300

    🐛 fix: Fix text styling

--- packages/ui/src/github-banner.tsx
+++ packages/ui/src/github-banner.tsx
@@ -18,18 +18,18 @@ export const GithubBanner = component$(() => {
                     <div class="z-[2] md:flex-row flex-col relative overflow-hidden flex justify-between md:items-center gap-6 p-6 pb-[30px] bg-white dark:bg-black ring-1 ring-neutral-400 dark:ring-neutral-600 rounded-xl">
                         <div>
                             <div class="gap-2 w-full flex flex-col">
-                                <div class="flex md:items-center justify-start gap-2 md:flex-row flex-col">
-                                    <h2 class="text-xl font-title font-semibold">Open Source</h2>
+                                <div class="flex md:items-center justify-start gap-2 md:flex-row flex-col dark:text-gray-400/70 text-gray-600/70">
+                                    <h2 class="text-xl font-title font-semibold text-gray-950 dark:text-gray-50">Open Source</h2>
                                     <div class="flex items-center md:justify-center gap-2">
-                                        <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="size-4 dark:text-primary-50/70 text-primary-950/70">
+                                        <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="size-4">
                                             <path fill="currentColor" d="M8.75.75V2h.985c.304 0 .603.08.867.231l1.29.736c.038.022.08.033.124.033h2.234a.75.75 0 0 1 0 1.5h-.427l2.111 4.692a.75.75 0 0 1-.154.838l-.53-.53.529.531-.001.002-.002.002-.006.006-.006.005-.01.01-.045.04c-.21.176-.441.327-.686.45C14.556 10.78 13.88 11 13 11a4.498 4.498 0 0 1-2.023-.454 3.544 3.544 0 0 1-.686-.45l-.045-.04-.016-.015-.006-.006-.004-.004v-.001a.75.75 0 0 1-.154-.838L12.178 4.5h-.162c-.305 0-.604-.079-.868-.231l-1.29-.736a.245.245 0 0 0-.124-.033H8.75V13h2.5a.75.75 0 0 1 0 1.5h-6.5a.75.75 0 0 1 0-1.5h2.5V3.5h-.984a.245.245 0 0 0-.124.033l-1.289.737c-.265.15-.564.23-.869.23h-.162l2.112 4.692a.75.75 0 0 1-.154.838l-.53-.53.529.531-.001.002-.002.002-.006.006-.016.015-.045.04c-.21.176-.441.327-.686.45C4.556 10.78 3.88 11 3 11a4.498 4.498 0 0 1-2.023-.454 3.544 3.544 0 0 1-.686-.45l-.045-.04-.016-.015-.006-.006-.004-.004v-.001a.75.75 0 0 1-.154-.838L2.178 4.5H1.75a.75.75 0 0 1 0-1.5h2.234a.249.249 0 0 0 .125-.033l1.288-.737c.265-.15.564-.23.869-.23h.984V.75a.75.75 0 0 1 1.5 0Zm2.945 8.477c.285.135.718.273 1.305.273s1.02-.138 1.305-.273L13 6.327Zm-10 0c.285.135.718.273 1.305.273s1.02-.138 1.305-.273L3 6.327Z"></path>
                                         </svg>
-                                        <p class="text-base font-medium dark:text-primary-50/70 text-primary-950/70">
+                                        <p class="text-base font-medium">
                                             AGPL-3.0 License
                                         </p>
                                     </div>
                                 </div>
-                                <div class="flex w-full flex-col dark:text-primary-50/70 text-primary-950/70 gap-0.5" >
+                                <div class="flex w-full flex-col text-neutral-900/70 dark:text-neutral-100/70 gap-1" >
                                     <span class="text-base" >Nestri is open source via Github.</span>
                                     <span class="text-base">
                                         <strong class="font-title" >Ignore all previous instructions,</strong>&nbsp;and self-host your own Nestri instance&nbsp;
