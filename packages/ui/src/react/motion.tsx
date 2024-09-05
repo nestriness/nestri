2e26ed08a1662cf4f358a280b846e94c8d8816e6
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2024-09-05 17:33:46 +0300

    ✨ fix: Change up the `/404` page

--- packages/ui/src/react/motion.tsx
+++ packages/ui/src/react/motion.tsx
@@ -24,13 +24,12 @@ export const ReactMotionComponent = ({
     class: className,
     ...motionProps
 }: MotionComponentProps) => {
-    const MotionTag = motion[as as keyof typeof motion];
+    const MotionTag = motion[as as keyof typeof motion] as React.ComponentType<any>;
 
     return (
-        <MotionTag className={className}
-            {...motionProps}
-            // animate={isInView ? whileInView : undefined}
-        >
+        <MotionTag
+            className={className}
+            {...(motionProps as any)}>
             {children}
         </MotionTag>
     );
