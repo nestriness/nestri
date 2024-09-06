c19981355630cf8b1f5c4b14d80fd6e45df04557
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2024-09-07 00:43:41 +0300

    ✨ feat:  Add `@qwik-ui/headless` for creating Qwik modal

--- apps/www/src/routes/index.tsx
+++ apps/www/src/routes/index.tsx
@@ -1,8 +1,9 @@
 import { component$ } from "@builder.io/qwik";
 import { Link, type DocumentHead } from "@builder.io/qwik-city";
-import { HeroSection, Cursor,  MotionComponent, transition } from "@nestri/ui/react"
+import { HeroSection, Cursor, MotionComponent, transition } from "@nestri/ui/react"
 import { NavBar, Footer } from "@nestri/ui"
 import { BasicImageLoader } from "@nestri/ui/image";
+import { Modal, Label } from '@qwik-ui/headless';
 
 const features = [
   {
@@ -90,22 +91,43 @@ export default component$(() => {
     <>
       <NavBar />
       <HeroSection client:load>
-        <button class="w-full max-w-xl rounded-xl flex items-center justify-between hover:bg-gray-200 dark:hover:bg-gray-800 transition-all gap-2 px-4 py-3 h-[45px] ring-2 ring-gray-300 dark:ring-gray-700 mx-auto text-gray-900/70 dark:text-gray-100/70 bg-white dark:bg-black">
-          <span class="flex items-center gap-3 h-max justify-center overflow-hidden overflow-ellipsis whitespace-nowrap">
-            <svg xmlns="http://www.w3.org/2000/svg" class="size-[18] flex-shrink-0" height="18" width="18" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="2"><circle cx="11.5" cy="11.5" r="9.5" /><path stroke-linecap="round" d="M18.5 18.5L22 22" /></g></svg>
-            Search for a game to play...
-          </span>
-          <span class="flex items-center gap-2">
-            <div class="flex items-center gap-2 text-base font-title font-bold">
-              <kbd class="ring-2 ring-gray-300 dark:ring-gray-700 px-2 py-1 rounded-md">
-                <svg xmlns="http://www.w3.org/2000/svg" class="size-5 flex-shrink-0" width="20" height="20" viewBox="0 0 256 256"><path fill="currentColor" d="M180 144h-20v-32h20a36 36 0 1 0-36-36v20h-32V76a36 36 0 1 0-36 36h20v32H76a36 36 0 1 0 36 36v-20h32v20a36 36 0 1 0 36-36m-20-68a20 20 0 1 1 20 20h-20ZM56 76a20 20 0 0 1 40 0v20H76a20 20 0 0 1-20-20m40 104a20 20 0 1 1-20-20h20Zm16-68h32v32h-32Zm68 88a20 20 0 0 1-20-20v-20h20a20 20 0 0 1 0 40" /></svg>
-              </kbd>
-              <span class="px-2 py-0.5 rounded-md ring-2 ring-gray-300 dark:ring-gray-700">
-                K
-              </span>
-            </div>
-          </span>
-        </button>
+        <Modal.Root class="w-full py-2">
+          <Modal.Trigger class="w-full outline-none focus:ring-primary-500 transition-all duration-200 max-w-xl rounded-xl flex items-center justify-between hover:bg-gray-200 dark:hover:bg-gray-800 gap-2 px-4 py-3 h-[45px] ring-2 ring-gray-300 dark:ring-gray-700 mx-auto text-gray-900/70 dark:text-gray-100/70 bg-white dark:bg-black">
+            <span class="flex items-center gap-3 h-max justify-center overflow-hidden overflow-ellipsis whitespace-nowrap">
+              <svg xmlns="http://www.w3.org/2000/svg" class="size-[18] flex-shrink-0" height="18" width="18" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="2"><circle cx="11.5" cy="11.5" r="9.5" /><path stroke-linecap="round" d="M18.5 18.5L22 22" /></g></svg>
+              Search for a game to play...
+            </span>
+            <span class="flex items-center gap-2">
+              <div class="flex items-center gap-2 text-base font-title font-bold">
+                <kbd class="ring-2 ring-gray-300 dark:ring-gray-700 px-2 py-1 rounded-md">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="size-5 flex-shrink-0" width="20" height="20" viewBox="0 0 256 256"><path fill="currentColor" d="M180 144h-20v-32h20a36 36 0 1 0-36-36v20h-32V76a36 36 0 1 0-36 36h20v32H76a36 36 0 1 0 36 36v-20h32v20a36 36 0 1 0 36-36m-20-68a20 20 0 1 1 20 20h-20ZM56 76a20 20 0 0 1 40 0v20H76a20 20 0 0 1-20-20m40 104a20 20 0 1 1-20-20h20Zm16-68h32v32h-32Zm68 88a20 20 0 0 1-20-20v-20h20a20 20 0 0 1 0 40" /></svg>
+                </kbd>
+                <span class="px-2 py-0.5 rounded-md ring-2 ring-gray-300 dark:ring-gray-700">
+                  K
+                </span>
+              </div>
+            </span>
+          </Modal.Trigger>
+          <Modal.Panel class="w-full backdrop:bg-white/30 dark:backdrop:bg-black/30 backdrop:backdrop-blur-md max-w-xl bg-white dark:bg-black rounded-xl p-4">
+            <Modal.Title>Edit Profile</Modal.Title>
+            <Modal.Description>
+              You can update your profile here. Hit the save button when finished.
+            </Modal.Description>
+            <Label>
+              Name
+              <input type="text" placeholder="John Doe" />
+            </Label>
+            <Label>
+              Email
+              <input type="text" placeholder="johndoe@gmail.com" />
+            </Label>
+            <footer>
+              <Modal.Close class="modal-close">Cancel</Modal.Close>
+              <Modal.Close class="modal-close">Save Changes</Modal.Close>
+            </footer>
+          </Modal.Panel>
+
+        </Modal.Root>
       </HeroSection>
       <MotionComponent
         initial={{ opacity: 0, y: 100 }}
