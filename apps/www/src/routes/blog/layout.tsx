516737bf23e42717fadae12720e80e9b8d6ff359
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2024-09-08 19:57:58 +0300

    ✨ feat: Add blog authors and time

--- apps/www/src/routes/blog/layout.tsx
+++ apps/www/src/routes/blog/layout.tsx
@@ -13,11 +13,9 @@ function getDaysAgo(date: Date): string {
 
 export default component$(() => {
     useStyles$(styles)
-    //How do i know if it is /blog/** and not /blog?
     const isBlogPost = useLocation().url.pathname.startsWith("/blog/")
     const { frontmatter } = useDocumentHead()
     const { headings } = useContent();
-    // console.log(headings)
 
     useOnDocument('load', $(() => {
         const sections = document.querySelectorAll('.blog h3');
@@ -49,21 +47,54 @@ export default component$(() => {
             {
                 isBlogPost ?
                     (
-                        <div class="w-full grid grid-cols-1 relative sm:grid-cols-[1fr_auto_1fr] items-center justify-center px-4 py-20 pb-10">
+                        <div class="gap-8 w-full sm:grid flex relative sm:grid-cols-[1fr_auto_1fr] items-center justify-center px-4 py-20 pb-10">
                             <div class="hidden sm:block h-full">
-                                <nav class="sticky top-20 z-10 w-full flex justify-center items-center">
+                                <nav class="sticky top-20 z-10 w-full flex flex-col justify-center px-8 items-end gap-8">
                                     <Link href="/blog" class="flex w-max justify-center gap-1 items-center text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100">
                                         <svg width="18px" height="18px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" color="currentColor"><path d="M10.25 4.75l-3.5 3.5 3.5 3.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M6.75 8.25h6a4 4 0 014 4v7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                                         Blog
                                     </Link>
+                                    {/* <ul id="toc" class="list-none p-0 m-0 flex flex-col items-start justify-start gap-1">
+                                        {headings?.map((heading) => (
+                                            <li key={heading.id} class="text-sm text-gray-600 dark:text-gray-400 relative transition-all duration-200">
+                                                <a href={`#${heading.id}`} class="flex flex-row items-center ml-4 gap-2 overflow-ellipsis text-nowrap hover:text-gray-900 dark:hover:text-gray-100">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 absolute left-0 hidden group-hover:block" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m7 6l-.112.006a1 1 0 0 0-.669 1.619L9.72 12l-3.5 4.375A1 1 0 0 0 7 18h6a1 1 0 0 0 .78-.375l4-5a1 1 0 0 0 0-1.25l-4-5A1 1 0 0 0 13 6z" /></svg>
+                                                    {heading.text}
+                                                </a>
+                                            </li>
+                                        ))}
+                                    </ul> */}
                                 </nav>
                             </div>
-                            <div class="max-w-xl mx-auto gap-1 flex flex-col">
+                            <div class="max-w-xl mx-auto w-full gap-1 flex flex-col">
                                 <h2 class="text-4xl text-start tracking-tight font-bold font-title">
                                     {frontmatter.blogTitle}
                                 </h2>
-                                <div class="list-disc flex flex-row items-center justify-start mt-2 text-sm">
-                                    <span class="text-base dark:text-neutral-400 text-neutral-600 text-nowrap">
+                                <div class="list-none flex flex-col items-start justify-start mt-2 gap-2 text-sm w-full">
+                                    <span class="text-base dark:text-neutral-400 text-neutral-600 text-nowrap flex flex-row w-full items-center overflow-ellipsis">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="size-5 mr-1" width="32" height="32" viewBox="0 0 24 24">
+                                            <g fill="none" fill-rule="evenodd">
+                                                <path d="M24 0v24H0V0zM12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036q-.016-.004-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z" />
+                                                <path fill="currentColor" d="M6 7a5 5 0 1 1 10 0A5 5 0 0 1 6 7m5-3a3 3 0 1 0 0 6a3 3 0 0 0 0-6M4.413 17.601c-.323.41-.413.72-.413.899c0 .118.035.232.205.384c.197.176.55.37 1.11.543c1.12.346 2.756.521 4.706.563a1 1 0 1 1-.042 2c-1.997-.043-3.86-.221-5.254-.652c-.696-.216-1.354-.517-1.852-.962C2.347 19.906 2 19.274 2 18.5c0-.787.358-1.523.844-2.139c.494-.625 1.177-1.2 1.978-1.69C6.425 13.695 8.605 13 11 13q.671 0 1.316.07a1 1 0 0 1-.211 1.989Q11.564 15 11 15c-2.023 0-3.843.59-5.136 1.379c-.647.394-1.135.822-1.45 1.222Zm16.8-3.567a2.5 2.5 0 0 0-3.536 0l-3.418 3.417a1.5 1.5 0 0 0-.424.849l-.33 2.308a1 1 0 0 0 1.133 1.133l2.308-.33a1.5 1.5 0 0 0 .849-.424l3.417-3.418a2.5 2.5 0 0 0 0-3.535Zm-2.122 1.414a.5.5 0 0 1 .707.707l-3.3 3.3l-.825.118l.118-.825z" /></g>
+                                        </svg>
+                                        {/* By&nbsp; */}
+                                        {frontmatter.authors?.map((author: any, index: number) => (
+                                            <>
+                                                &nbsp;
+                                                <Link href={author.link} class="underline underline-offset-4 hover:text-gray-900 dark:hover:text-gray-100" key={author.name}>
+                                                    {author.name}
+                                                </Link>
+                                                &nbsp;
+                                                {/* {author.name !== frontmatter.authors[frontmatter.authors.length - 1].name && ', '} */}
+                                                {index < frontmatter.authors.length - 2 && ', '}
+                                                {index === frontmatter.authors.length - 2 && (frontmatter.authors.length > 2 ? ', and ' : ' and ')}
+                                            </>
+                                        ))}
+                                    </span>
+                                    <span class="text-base dark:text-neutral-400 items-center flex flex-row w-full overflow-ellipsis text-neutral-600 text-nowrap">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="size-5 mr-1" width="32" height="32" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><circle cx="12" cy="13" r="8" /><path d="M12 9v4l2 2M5 3L2 6m20 0l-3-3M6.38 18.7L4 21m13.64-2.33L20 21" /></g></svg>
+                                        {/* On&nbsp; */}
+                                        &nbsp;
                                         {new Date(frontmatter.createdAt).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })}
                                         &nbsp;
                                         {getDaysAgo(new Date(frontmatter.createdAt))}
@@ -78,8 +109,9 @@ export default component$(() => {
                                 <nav class="sticky top-20 z-10 w-full flex justify-center items-center">
                                     <ul id="toc" class="list-none p-0 m-0 flex flex-col items-start justify-start gap-1">
                                         {headings?.map((heading) => (
-                                            <li key={heading.id} class="text-sm text-gray-600 dark:text-gray-400">
-                                                <a href={`#${heading.id}`} class="hover:text-gray-900 dark:hover:text-gray-100">
+                                            <li key={heading.id} class="text-sm text-gray-600 dark:text-gray-400 relative transition-all duration-200">
+                                                <a href={`#${heading.id}`} class="flex flex-row items-center ml-4 gap-2 overflow-ellipsis text-nowrap hover:text-gray-900 dark:hover:text-gray-100">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 absolute left-0 hidden group-hover:block" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m7 6l-.112.006a1 1 0 0 0-.669 1.619L9.72 12l-3.5 4.375A1 1 0 0 0 7 18h6a1 1 0 0 0 .78-.375l4-5a1 1 0 0 0 0-1.25l-4-5A1 1 0 0 0 13 6z" /></svg>
                                                     {heading.text}
                                                 </a>
                                             </li>
