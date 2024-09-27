ff8cc72bd43c12d05046c8946d14e0a2bc1d87af
Victor Pahuus Petersen <49293748+victorpahuus@users.noreply.github.com>
2024-09-27 09:18:07 +0200

    Fixed build

--- apps/docs/README.md
+++ apps/docs/README.md
@@ -1,17 +1,27 @@
-# Docus
+# Docus Starter
+
+Starter template for [Docus](https://docus.dev).
+
+## Clone
+
+Clone the repository (using `nuxi`):
+
+```bash
+npx nuxi init -t themes/docus
+```
 
 ## Setup
 
 Install dependencies:
 
 ```bash
-npm install
+yarn install
 ```
 
 ## Development
 
 ```bash
-npm run dev
+yarn dev
 ```
 
 ## Edge Side Rendering
@@ -21,7 +31,7 @@ Can be deployed to Vercel Functions, Netlify Functions, AWS, and most Node-compa
 Look at all the available presets [here](https://v3.nuxtjs.org/guide/deploy/presets).
 
 ```bash
-npm build
+yarn build
 ```
 
 ## Static Generation
@@ -31,7 +41,7 @@ Use the `generate` command to build your application.
 The HTML files will be generated in the .output/public directory and ready to be deployed to any static compatible hosting.
 
 ```bash
-npm run generate
+yarn generate
 ```
 
 ## Preview build
--- apps/docs/nuxt.config.ts
+++ apps/docs/nuxt.config.ts
@@ -1,39 +1,7 @@
-import { createResolver, logger, defineNuxtModule } from '@nuxt/kit'
-import { $fetch } from 'ofetch'
-import { version } from './package.json'
-
-const { resolve } = createResolver(import.meta.url)
-
-// That allows to overwrite these dependencies paths via `.env` for local development
-const envModules = {
-  tokens: process?.env?.THEME_DEV_TOKENS_PATH || '@nuxt-themes/tokens',
-  elements: process?.env?.THEME_DEV_ELEMENTS_PATH || '@nuxt-themes/elements',
-  studio: process?.env?.THEME_DEV_STUDIO_PATH || '@nuxthq/studio',
-  typography: process?.env?.THEME_DEV_TYPOGRAPHY_PATH || '@nuxt-themes/typography'
-}
-
-const updateModule = defineNuxtModule({
-  meta: {
-    name: '@nuxt-themes/docus'
-  },
-  setup (_, nuxt) {
-    if (nuxt.options.dev) {
-      $fetch('https://registry.npmjs.org/@nuxt-themes/docus/latest').then((release) => {
-        if (release.version > version) {
-          logger.info(`A new version of Docus (v${release.version}) is available: https://github.com/nuxt-themes/docus/releases/latest`)
-        }
-      }).catch(() => {})
-    }
-  }
-})
-
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: ['@nuxt-themes/docus'],
-
   devtools: { enabled: true },
 
-
-
-  compatibilityDate: '2024-09-26'
+  
 })
