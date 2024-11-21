f49a4235c07a2b630828b30e8c9cbb0aa2819d53
Wanjohi <elviswanjohi47@gmail.com>
2024-11-21 12:19:28 +0300

    feat: Add the cargo server

--- packages/relay/dev/src/main.rs
+++ packages/relay/dev/src/main.rs
@@ -1,6 +1,179 @@
+use reqwest;
+use serde::{Deserialize, Serialize};
+use std::io;
+use std::sync::Arc;
+use tokio::time::Duration;
+use webrtc::api::interceptor_registry::register_default_interceptors;
+use webrtc::api::media_engine::MediaEngine;
+use webrtc::api::APIBuilder;
+use webrtc::data_channel::data_channel_message::DataChannelMessage;
+use webrtc::ice_transport::ice_server::RTCIceServer;
+use webrtc::interceptor::registry::Registry;
+use webrtc::peer_connection::configuration::RTCConfiguration;
+use webrtc::peer_connection::math_rand_alpha;
+use webrtc::peer_connection::peer_connection_state::RTCPeerConnectionState;
+use webrtc::peer_connection::sdp::sdp_type::RTCSdpType;
+use webrtc::peer_connection::sdp::session_description::RTCSessionDescription;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct SessionDescription {
+    #[serde(rename = "type")]
+    type_: String,
+    sdp: String,
+}
+
+fn map_to_io_error<E: std::fmt::Display>(e: E) -> io::Error {
+    io::Error::new(io::ErrorKind::Other, format!("{}", e))
+}
 
 #[tokio::main]
-async fn main()  {
-Ok(())
+async fn main() -> std::io::Result<()> {
+    let relay_url = "http://localhost:8088";
+    let stream_name = "test";
+    let url = format!("{}/api/whep/{}", relay_url, stream_name);
+
+    // Create a MediaEngine object to configure the supported codec
+    let mut m = MediaEngine::default();
+
+    // Register default codecs
+    let _ = m.register_default_codecs().map_err(map_to_io_error)?;
+
+    let mut registry = Registry::new();
+
+    // Use the default set of Interceptors
+    registry = register_default_interceptors(registry, &mut m).map_err(map_to_io_error)?;
+
+    // Create the API object with the MediaEngine
+    let api = APIBuilder::new()
+        .with_media_engine(m)
+        .with_interceptor_registry(registry)
+        .build();
+
+    // Prepare the configuration
+    let config = RTCConfiguration {
+        ice_servers: vec![RTCIceServer {
+            urls: vec!["stun:stun.l.google.com:19302".to_owned()],
+            ..Default::default()
+        }],
+        ..Default::default()
+    };
+
+    // Create a new RTCPeerConnection
+    let peer_connection = Arc::new(
+        api.new_peer_connection(config)
+            .await
+            .map_err(map_to_io_error)?,
+    );
+
+    // Create a datachannel with label 'data'
+    let data_channel = peer_connection
+        .create_data_channel("data", None)
+        .await
+        .map_err(map_to_io_error)?;
+
+    let (done_tx, mut done_rx) = tokio::sync::mpsc::channel::<()>(1);
+
+    // Set the handler for Peer connection state
+    // This will notify you when the peer has connected/disconnected
+    peer_connection.on_peer_connection_state_change(Box::new(move |s: RTCPeerConnectionState| {
+        println!("Peer Connection State has changed: {s}");
+
+        if s == RTCPeerConnectionState::Failed {
+            // Wait until PeerConnection has had no network activity for 30 seconds or another failure. It may be reconnected using an ICE Restart.
+            // Use webrtc.PeerConnectionStateDisconnected if you are interested in detecting faster timeout.
+            // Note that the PeerConnection may come back from PeerConnectionStateDisconnected.
+            println!("Peer Connection has gone to failed exiting");
+            let _ = done_tx.try_send(());
+        }
+
+        Box::pin(async {})
+    }));
+
+    // Register channel opening handling
+    let d1 = Arc::clone(&data_channel);
+    data_channel.on_open(Box::new(move || {
+        println!("Data channel '{}'-'{}' open. Random messages will now be sent to any connected DataChannels every 5 seconds", d1.label(), d1.id());
+
+        let d2 = Arc::clone(&d1);
+        Box::pin(async move {
+            let mut result = std::io::Result::<usize>::Ok(0);
+            while result.is_ok() {
+                let timeout = tokio::time::sleep(Duration::from_secs(5));
+                tokio::pin!(timeout);
+
+                tokio::select! {
+                    _ = timeout.as_mut() =>{
+                        let message = math_rand_alpha(15);
+                        println!("Sending '{message}'");
+                        result = d2.send_text(message).await.map_err(map_to_io_error);
+                    }
+                };
+            }
+        })
+    }));
+
+    // Register text message handling
+    let d_label = data_channel.label().to_owned();
+    data_channel.on_message(Box::new(move |msg: DataChannelMessage| {
+        let msg_str = String::from_utf8(msg.data.to_vec()).unwrap();
+        println!("Message from DataChannel '{d_label}': '{msg_str}'");
+        Box::pin(async {})
+    }));
+
+    // Create an offer to send to the browser
+    let offer = peer_connection
+        .create_offer(None)
+        .await
+        .map_err(map_to_io_error)?;
+
+    // Create channel that is blocked until ICE Gathering is complete
+    let mut gather_complete = peer_connection.gathering_complete_promise().await;
+
+    // Sets the LocalDescription, and starts our UDP listeners
+    peer_connection
+        .set_local_description(offer)
+        .await
+        .map_err(map_to_io_error)?;
+
+    // Block until ICE Gathering is complete, disabling trickle ICE
+    // we do this because we only can exchange one signaling message
+    // in a production application you should exchange ICE Candidates via OnICECandidate
+    let _ = gather_complete.recv().await;
+
+    if let Some(local_description) = peer_connection.local_description().await {
+        let response = reqwest::Client::new()
+            .post(&url)
+            .header("Content-Type", "application/sdp")
+            .body(local_description.sdp.clone()) // clone if you don't want to move offer.sdp
+            .send()
+            .await
+            .map_err(map_to_io_error)?;
+
+        let answer = response
+            .json::<RTCSessionDescription>()
+            .await
+            .map_err(map_to_io_error)?;
+
+        peer_connection
+            .set_remote_description(answer)
+            .await
+            .map_err(map_to_io_error)?;
+    } else {
+        println!("generate local_description failed!");
+    };
+
+    println!("Press ctrl-c to stop");
+    
+    tokio::select! {
+        _ = done_rx.recv() => {
+            println!("received done signal!");
+        }
+        _ = tokio::signal::ctrl_c() => {
+            println!();
+        }
+    };
+
+    peer_connection.close().await.map_err(map_to_io_error)?;
+
+    Ok(())
 }
