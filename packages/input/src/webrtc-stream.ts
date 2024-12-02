5281a314db75c95455440e9ca8acda6791458d66
DatCaptainHorse <DatCaptainHorse@users.noreply.github.com>
2024-12-02 18:23:15 +0200

    feat: Allow front to reconnect on relay loss

--- packages/input/src/webrtc-stream.ts
+++ packages/input/src/webrtc-stream.ts
@@ -25,51 +25,16 @@ export class WebRTCStream {
     }
 
     this._onConnected = connectedCallback;
+    this._setup(serverURL, roomName);
+  }
 
+  private _setup(serverURL: string, roomName: string) {
     console.log("Setting up WebSocket");
     // Replace http/https with ws/wss
     const wsURL = serverURL.replace(/^http/, "ws");
     this._ws = new WebSocket(`${wsURL}/api/ws/${roomName}`);
     this._ws.onopen = async () => {
       console.log("WebSocket opened");
-
-      console.log("Setting up PeerConnection");
-      this._pc = new RTCPeerConnection({
-        iceServers: [
-          {
-            urls: "stun:stun.l.google.com:19302"
-          }
-        ],
-      });
-
-      this._pc.ontrack = (e) => {
-        console.log("Track received: ", e.track);
-        this._mediaStream = e.streams[e.streams.length - 1];
-      };
-
-      this._pc.onconnectionstatechange = () => {
-        console.log("Connection state: ", this._pc!.connectionState);
-        if (this._pc!.connectionState === "connected") {
-          if (this._onConnected && this._mediaStream)
-            this._onConnected(this._mediaStream);
-        }
-      };
-
-      this._pc.onicecandidate = (e) => {
-        if (e.candidate) {
-          const message: MessageICE = {
-            payload_type: "ice",
-            candidate: e.candidate
-          };
-          this._ws!.send(encodeMessage(message));
-        }
-      }
-
-      this._pc.ondatachannel = (e) => {
-        this._dataChannel = e.channel;
-        this._setupDataChannelEvents();
-      }
-
       // Send join message
       const joinMessage: MessageJoin = {
         payload_type: "join",
@@ -87,24 +52,30 @@ export class WebRTCStream {
       const message = await decodeMessage<MessageBase>(e.data);
       switch (message.payload_type) {
         case "sdp":
-          await this._pc!.setRemoteDescription((message as MessageSDP).sdp);
+          if (!this._pc) {
+            // Setup peer connection now
+            this._setupPeerConnection();
+          }
+          console.log("Received SDP: ", (message as MessageSDP).sdp);
+          await this._pc.setRemoteDescription((message as MessageSDP).sdp);
           // Create our answer
-          const answer = await this._pc!.createAnswer();
+          const answer = await this._pc.createAnswer();
           // Force stereo in Chromium browsers
           answer.sdp = this.forceOpusStereo(answer.sdp!);
-          await this._pc!.setLocalDescription(answer);
+          await this._pc.setLocalDescription(answer);
           this._ws!.send(encodeMessage({
             payload_type: "sdp",
             sdp: answer
           }));
           break;
         case "ice":
+          if (!this._pc) break;
           // If remote description is not set yet, hold the ICE candidates
-          if (this._pc!.remoteDescription) {
-            await this._pc!.addIceCandidate((message as MessageICE).candidate);
+          if (this._pc.remoteDescription) {
+            await this._pc.addIceCandidate((message as MessageICE).candidate);
             // Add held ICE candidates
             for (const ice of iceHolder) {
-              await this._pc!.addIceCandidate(ice);
+              await this._pc.addIceCandidate(ice);
             }
             iceHolder = [];
           } else {
@@ -134,11 +105,62 @@ export class WebRTCStream {
     }
 
     this._ws.onclose = () => {
-      console.log("WebSocket closed");
+      console.log("WebSocket closed, reconnecting in 3 seconds");
+      if (this._onConnected)
+        this._onConnected(null);
+
+      // Clear PeerConnection
+      if (this._pc) {
+        this._pc.close();
+        this._pc = undefined;
+      }
+
+      setTimeout(() => {
+        this._setup(serverURL, roomName);
+      }, 3000);
     }
 
     this._ws.onerror = (e) => {
       console.error("WebSocket error: ", e);
+    }
+  }
+
+  private _setupPeerConnection() {
+    console.log("Setting up PeerConnection");
+    this._pc = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: "stun:stun.l.google.com:19302"
+        }
+      ],
+    });
+
+    this._pc.ontrack = (e) => {
+      console.log("Track received: ", e.track);
+      this._mediaStream = e.streams[e.streams.length - 1];
+    };
+
+    this._pc.onconnectionstatechange = () => {
+      console.log("Connection state: ", this._pc!.connectionState);
+      if (this._pc!.connectionState === "connected") {
+        if (this._onConnected && this._mediaStream)
+          this._onConnected(this._mediaStream);
+      }
+    };
+
+    this._pc.onicecandidate = (e) => {
+      if (e.candidate) {
+        const message: MessageICE = {
+          payload_type: "ice",
+          candidate: e.candidate
+        };
+        this._ws!.send(encodeMessage(message));
+      }
+    }
+
+    this._pc.ondatachannel = (e) => {
+      this._dataChannel = e.channel;
+      this._setupDataChannelEvents();
     }
   }
 
